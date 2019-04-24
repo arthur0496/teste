4052aedaf9838898ecc22f2c4254e288063d0f2b
Arthur Assis <arthur120496@gmail.com>
2019-04-24 10:15:49 -0300

    asdghkjas

---
+++ test2.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread("trash_on_water.jpg", cv2.IMREAD_COLOR)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)[...,0]
+# edges = cv2.Canny(gray, 10,30)    
+blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+clahe = cv2.createCLAHE(clipLimit=5.0, tileGridSize=(32,32))
+contrast = clahe.apply(blurred)
+ret, thresh = cv2.threshold(contrast, 20, 255, cv2.THRESH_BINARY|cv2.THRESH_OTSU)
+plt.imshow(thresh)
+plt.savefig('./threshold.png')
